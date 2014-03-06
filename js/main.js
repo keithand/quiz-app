2d46e3fcedd740d0601aadae3441a98ee032bd4c
Keith Anderson <keithanderson@boldier.com>
2014-03-06 11:06:29 -0800

    Version 1.4

--- js/main.js
+++ js/main.js
@@ -25,34 +25,57 @@ $(document).ready(function() {
 	];
 
 	// ALERT FOR A NEW GAME TO START
-	// AND START FIRST QUESTION
+	// AND START QUESTION DISPLAY AND ANSWER
 
-	var newGame = function  (){
+	var r = $.Deferred();
+
+	var newGame = function  (event){
 		alert("Are you ready to begin?");
+		return r
+	}
+
+
+	var questionOne = function (){
+
 		$("#title").text(question[0].num);
 		$("#quote q").text(question[0].quote);
 		$("#bartradio").on('click', function(event){
 			alert("Correct!");
+			$(this).preventDefault();
 		});
 		$("#parisradio, #georgeradio").on("click", function(event){
 			alert("Incorrect.");
-
 		});
-		return true
-	}
-
-		newGame();
-
-	// AFTER FIRST QUESTION, LOAD NEXT QUESTIONS
-
-	var nextQuestion = function (){
-		if(newGame === true){
-			alert("AHH@");
-		}
+		return r
 	};
 
-	nextQuestion();
+	var questionTwo = function (){
 
+	$("#title").text(question[1].num);
+	$("#quote q").text(question[1].quote);
+	$("#bartradio").on('click', function(event){
+		alert("Correct!");
+	});
+	$("#parisradio, #georgeradio").on("click", function(event){
+		alert("Incorrect.");
+	});
+	return r
+}
 
+	var questionThree = function (){
+
+	$("#title").text(question[2].num);
+	$("#quote q").text(question[2].quote);
+	$("#georgeradio").on('click', function(event){
+		alert("Correct!");
+	});
+	$("#parisradio, #bartradio").on("click", function(event){
+		alert("Incorrect.");
+	});
+	return r
+}
+	questionOne();
+	questionTwo();
+	questionThree();
 
 });
