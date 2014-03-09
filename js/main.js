8f0814ad899d5c5235bfb5d79219564aaa3a1d88
Keith Anderson <keithanderson@boldier.com>
2014-03-09 16:26:48 -0700

    Version 1.6
    Still working on it…

--- js/main.js
+++ js/main.js
@@ -1,87 +1,82 @@
 $(document).ready(function() {
 	
+	//SET VARIABLES TO COUNT ANSWERS
+
+	var questionsAnswered = 0;
+	var guessCount = 0;
+	var result;
+	var qnum = 0;
+
+
+	var paris = $("#parisradio").checked;
+	var george = $("#georgeradio").checked;
+	var bart = $("#bartradio").checked;
+
+	function clicked(){
+		if(bart === true || paris === true || george === true){
+			$(this).addClass('selected');
+		} if (bart === false || paris === false || george === false){
+			$(this).removeClass('selected');
+		}
+	};
+
+
 	// SET THE QUESTION DATA VARIABLE
 
-	var question = [{
-		num: "Question #1",
+	var question = [
+		{
+		num: 1,
 		quote : "Aren't we forgetting the true meaning of Christmas: the birth of Santa.",
-		person : "Bart Simpson"
+		ans : bart
 		},
 		{
-		num: "Question #2",
+		num: 2,
 		quote : "I know how hard it is for you to put food on your family.",
-		person: "George Bush"
+		ans: george
 		},
 		{
-		num: "Question #3",
+		num: 3,
 		quote: "They misunderestimated me.",
-		person : "George Bush"
+		ans : george
 		},
 		{
-		num: "Question #4",
+		num: 4,
 		quote: "I don't think there's ever been anyone like me that's lasted. And I'm going to keep lasting.",
-		person : "Paris Hilton"
+		ans : paris
 		}
 	];
 
 	// AND START QUESTION DISPLAY AND ANSWER
 
-	var newGame = function  (event){
-		alert("Are you ready to begin?");
-	}
+	var quiz = function (){
+				
+			var reset = function(){
+				qnum = 0;
+				nextQuestion();
+			};
 
+			var nextQuestion = function (){
+				$("#title").text("This is question " + question[qnum].num);
+				$("#quote q").text(question[qnum].quote);
 
-	var questionOne = function (event){
+				if(nextQuestion >= 0) {
+					$("#title").text("Question #" + question[0].num);
+					$("#quote q").text(question[0].quote);
+					if (question[qnum].ans === true){
+						alert("good job!");
+					} else {
+						alert("You're wrong!");
+					}
+				qnum++;
 
-		$("#title").text(question[0].num);
-		$("#quote q").text(question[0].quote);
-		$("#bartradio").on('click', function(event){
-			alert("Correct!");
-			return true
-		});
-		$("#parisradio, #georgeradio").on("click", function(event){
-			alert("Incorrect.");
-		return true
-		});
+				}
+			reset();
+
+			};
+
 	};
 
-	var questionTwo = function (){
+quiz();
 
-	$("#title").text(question[1].num);
-	$("#quote q").text(question[1].quote);
-	$("#bartradio").on('click', function(event){
-		alert("Correct!");
-		return true
-	});
-	$("#parisradio, #georgeradio").on("click", function(event){
-		alert("Incorrect.");
-		return true
-	});
-}
-
-	var questionThree = function (){
-
-	$("#title").text(question[2].num);
-	$("#quote q").text(question[2].quote);
-	$("#georgeradio").on('click', function(event){
-		alert("Correct!");
-		return true
-	});
-	$("#parisradio, #bartradio").on("click", function(event){
-		alert("Incorrect.");
-		return true
-	});
-}
-	function questionSeries () {
-		questionOne();
-		if(questionOne === true){
-		questionTwo()
-	}; if (questionTwo === true) {
-		questionThree()
-	}
-
-}
-
-questionSeries();
 
 });
