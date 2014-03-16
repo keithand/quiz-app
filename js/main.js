bce15dbdd208cfc1442afc558e25c2cf5f9454d5
Keith Anderson <keithanderson@boldier.com>
2014-03-16 15:19:51 -0700

    Version 2.0

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ var score = 0;
 var response; // the users answer
 var qnum = 0; // keeps track of what question number the user is on
 var result; //the value if the user answered correctly
-var answered; //stores the value of whether the user has already answered the question
+var answered = 0; //stores the value of whether the user has already answered the question
 
 var correctAnswer = 0;
 var wrongAnswer = 0;
@@ -39,36 +39,54 @@ var question = [
 		quote: "I don't think there's ever been anyone like me that's lasted. And I'm going to keep lasting.",
 		ans : 'Paris Hilton',
 		correct: 0
+		},
+		{
+		num: 5,
+		quote: "I get half a million just to show up at parties. My life is, like, really, really fun!",
+		ans: 'Paris Hilton',
+		correct: 0
 		}
 	];
 
+	var currentQuestionData = question[qnum];
+	var userAnswer = $("input[type='radio']:checked").index("input[type='radio']");
+
 	//DISPLAY CURRENT QUESTION//
-	 displayCurrentQuestion = function (qnum, question){
-		var currentQuestionData = question[qnum];
+	 displayCurrentQuestion = function (){
 		var userAnswer = $("input[type='radio']:checked").index("input[type='radio']");
+		currentQuestionData = question[qnum];
 		$('#quest_num').text(currentQuestionData.num);
 		$('#quote q').text(currentQuestionData.quote);
 	};
 
-		var currentQuestionData = question[qnum];
-		var userAnswer = $("input[type='radio']:checked").index("input[type='radio']");
 		
 		//EVENT HANDLER FOR SUBMIT BUTTON
-		$('#submit').on('click', function(qnum, question){ 
-		
-				if ( userAnswer == currentQuestionData.correct){
-					alert("Good job!");
+		$('#submit').on('click', function(){ 
+		userAnswer = $("input[type='radio']:checked").index("input[type='radio']");
+				
+				 if ( userAnswer == currentQuestionData.correct){
+					$("#response h2").text("Correct!");
+					$("#response p").text("");
+
 					correctAnswer++;
+					answered++;
+				
 					$("#correct").text(correctAnswer);
+					$("#answered").text(answered);
+
 				} else {
-					alert("Youre wrong.");
+					$("#response h2").text("Incorrect.");
+					$("#response p").text("The correct answer for question #" + (answered + 1) + " was " + question[qnum].ans + ".");					
+				
 					wrongAnswer++;
+					answered++;
+				
 					$("#incorrect").text(wrongAnswer);
+					$("#answered").text(answered);
 				}
 			qnum++;
 			displayCurrentQuestion(qnum, question);
 
+
 		});
-
-
 });
